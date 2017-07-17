3d31e5e0808b61c0fed27b973a22f065c5dbae0d
Wilson Wu <wilson@spatial.is>
2017-07-17 17:31:47 -0400

    put this stuff in unity

---
+++ Daydream-Nodejs/DaydreamServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using WebSocketSharp;
+
+public class DaydreamServer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WebSocket ws = new WebSocket("ws://localhost:5555");
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Server says: " + e.Data);
+        };
+        ws.ConnectAsync();
+        /*
+        using (var ws = new WebSocket("ws://localhost:5555"))
+        {
+            ws.Connect();
+            ws.OnMessage += (sender,e) =>
+            {
+                Debug.Log("Server says: " + e.Data);
+            };
+            ws.Send("nuggets");
+        }
+        */
+    }
+}
